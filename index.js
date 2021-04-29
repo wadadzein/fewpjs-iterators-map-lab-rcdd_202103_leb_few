3c243e610f9f7fb4c0fafceea62735d4d15b1249
wadad zein <wadad_zein@hotmail.com>
2021-04-29 18:05:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titlecased=()=>tutorials.map(tutorial=>tutorial.split('').map(word=>word[0].toUpperCase()+word.slice(1)).join('')
+const titlecased= () => tutorials.map(tutorial => tutorial.split(' ').map(
+  word => word[0].toUpperCase() + word.slice(1)
+  ).join(' ')
 );
 
 
-// const titleCased = () => tutorials.map(tutorial => tutorial.split(" ").map(
-//   word => word[0].toUpperCase() + word.slice(1)
-// ).join(' ')
+const titleCased = () => tutorials.map(tutorial => tutorial.split(" ").map(
+  word => word[0].toUpperCase() + word.slice(1)
+).join(' ')
 
-// );
+);
